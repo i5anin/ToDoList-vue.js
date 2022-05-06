767fe28ecf0ebaaaa72cb74ee3752ec6073819e6
Isanin Sergey <92434473+Quantum-PizZzA@users.noreply.github.com>
2022-05-06 14:42:15 +0300

    Authentication No Work

--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import App from "./App.vue";
 import router from "./router";
 import "materialize-css/dist/js/materialize.min";
 import "./style.css";
+import firebase from "firebase/app";
 
 createApp(App).use(router).mount("#app");
--- src/views/Authentication/Register.vue
+++ src/views/Authentication/Register.vue
@@ -1,9 +1,32 @@
-<template>Register</template>
+<template>
+  <h1>Создать учетную запись</h1>
+  <p><input type="text" placeholder="Email" v-model="email" /></p>
+  <p><input type="password" placeholder="Пароль" v-model="password" /></p>
+  <p>
+    <button class="waves-effect waves-light btn" @click="register">
+      Отправить
+    </button>
+  </p>
+</template>
 
-<script>
-export default {
-  name: "registr",
+<script setup>
+import { ref } from "vue";
+import { initializeApp } from "firebase/app";
+import { useRouter } from "vue-router"; // import router
+const email = ref("");
+const password = ref("");
+const router = useRouter(); // get a reference to our vue router
+const register = () => {
+  firebase
+    .auth() // get the auth api
+    .createUserWithEmailAndPassword(email.value, password.value) // need .value because ref()
+    .then((data) => {
+      console.log("Successfully registered!");
+      router.push("/feed"); // redirect to the feed
+    })
+    .catch((error) => {
+      console.log(error.code);
+      alert(error.message);
+    });
 };
 </script>
-
-<style scoped></style>
